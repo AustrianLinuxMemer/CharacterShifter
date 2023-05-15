52d5dccadb030dfc9389edecd357ea6ce9d4aa47
AustrianLinuxMemer <52295631+AustrianLinuxMemer@users.noreply.github.com>
2023-05-15 14:06:27 +0200

    Update Converter.java

--- src/Converter.java
+++ src/Converter.java
@@ -1,16 +1,11 @@
-import java.util.HashMap;
-import java.util.Map;
-
 public class Converter {
     public static char convertCharFromNormalToWidespace(char normal, int shift) {
-        int normal1 = normal + shift;
-        return (char) normal1;
+        return (char) (normal + shift);
     }
     public static String convertString(String string, int shift) {
         char[] charredString = string.toCharArray();
-        char[] newCharredString = new char[charredString.length];
         for (int i = 0; i < charredString.length; i++) {
-            newCharredString[i] = convertCharFromNormalToWidespace(charredString[i], shift);
+            charredString[i] = convertCharFromNormalToWidespace(charredString[i], shift);
         }
         return String.copyValueOf(newCharredString);
     }
