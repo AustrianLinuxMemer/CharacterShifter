113eacbdfe229007e7b234db8d822bf1fe01af5c
leo <leo.fuereder@outlook.com>
2023-05-15 14:10:18 +0200

    Release Alpha 1.0 candidate

--- src/Converter.java
+++ src/Converter.java
@@ -1,11 +1,8 @@
 public class Converter {
-    public static char convertCharFromNormalToWidespace(char normal, int shift) {
-        return (char) (normal + shift);
-    }
     public static String convertString(String string, int shift) {
         char[] charredString = string.toCharArray();
         for (int i = 0; i < charredString.length; i++) {
-            charredString[i] = convertCharFromNormalToWidespace(charredString[i], shift);
+            charredString[i] = (char) (charredString[i] + shift);
         }
         return String.copyValueOf(charredString);
     }
