4cd9dc5a83d99e69fc384373c41a3a82ef00c5b8
leo <leo.fuereder@outlook.com>
2023-05-15 13:47:45 +0200

    Major Changes

--- src/Converter.java
+++ src/Converter.java
@@ -2,41 +2,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Converter {
-    public static char convertCharFromNormalToWidespace(char normal) {
-        int normal1 = normal + 65248;
+    public static char convertCharFromNormalToWidespace(char normal, int shift) {
+        int normal1 = normal + shift;
         return (char) normal1;
     }
-    public static char convertCharFromWidespaceToNormal(char widespace){
-        int widespace1 = widespace - 65248;
-        return (char) widespace1;
-    }
-    public static boolean isValidNormalCharacter(char character){
-        return character >= 32 && character <= 127;
-    }
-    public static boolean isValidWidespaceCharacter(char character){
-        return character >= 32 + 65248 && character <= 127 + 65248;
-    }
-    public static String convertString(String string) {
+    public static String convertString(String string, int shift) {
         char[] charredString = string.toCharArray();
         char[] newCharredString = new char[charredString.length];
         for (int i = 0; i < charredString.length; i++) {
-            if (isValidNormalCharacter(charredString[i])) {
-                newCharredString[i] = convertCharFromNormalToWidespace(charredString[i]);
-            } else if (isValidWidespaceCharacter(charredString[i])) {
-                newCharredString[i] = convertCharFromWidespaceToNormal(charredString[i]);
-            } else {
-                System.exit(1);
-            }
+            newCharredString[i] = convertCharFromNormalToWidespace(charredString[i], shift);
         }
-        String toBeReturned = whitespaceCorrection(String.copyValueOf(newCharredString));
-        return toBeReturned;
-    }
-    public static String whitespaceCorrection(String toBeCorrected) {
-        String correctedString = "";
-        String[] splinter = toBeCorrected.split("\uFF00");
-        for (int i = 0; i < splinter.length; i++) {
-            correctedString = correctedString.concat(splinter[i] + "  ");
-        }
-        return correctedString;
+        return String.copyValueOf(newCharredString);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        if (args.length > 1) {
+        int shift = 65248;
+        if (args.length < 1 || args.length > 2) {
             System.exit(1);
+        } else if (args.length == 1) {
+            System.out.println(Converter.convertString(args[0], shift));
+        } else if (args.length == 2) {
+            shift = Integer.parseInt(args[1]);
+            System.out.println(Converter.convertString(args[0], shift));
         }
-        System.out.println(Converter.convertString(args[0]));
     }
 }
