0c3169fa7f951179037ff782da08bc1fba4a4304
leo <leo.fuereder@outlook.com>
2023-05-15 14:41:48 +0200

    Release Alpha 1.0 candidate v2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int shift = 65248;
         if (args.length < 1 || args.length > 2) {
-            System.out.println("Usage: CharacterShifter.jar String [Decimal shift value]");
+            System.out.println("Usage: CharacterShifter.jar <string> [shift]");
             System.exit(1);
         } else if (args.length == 1) {
             System.out.println(Converter.convertString(args[0], shift));
